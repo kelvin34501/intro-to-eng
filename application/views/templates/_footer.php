493af956b3d52000463d6f57ce698ab9474cb85d
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-05-31 12:10:04 +0800

    New footer for the project. After project structure is corrected, remove the '_' to enable it. 'data' variable should also be passed into the ->view('footer', data). There might be future updates.

---
+++ application/views/templates/_footer.php
@@ -0,0 +1,15 @@
+<footer class="footer navbar-inverse <?php if($title=="Home") echo "navbar-fixed-bottom" ?>">
+    <div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 centering text-gray">
+		<br><br>
+		<p>Designed an built with all the love in the world by Xinyu Zhan, Zhongye Wang, Yichen Xie, Ye Yue.</p>
+		<br>
+		</div>
+	</div>
+    </div>
+</footer>
+
+                </div>
+        </body>
+</html>
