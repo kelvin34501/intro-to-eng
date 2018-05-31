52e062aceab778549ba4dc2319ffd64676f6c48a
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-05-31 12:48:19 +0800

    I added this as the frame of the result page. There will be future updates.

---
+++ application/views/lab/result.frame.php
@@ -0,0 +1,51 @@
+<div class="container-fluid">
+<br><br>
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<div class="alert alert-info alert-dismissable">
+					 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+				<h4 class="text-center"><?php echo $total_result; ?> matches were found in database.</h4>
+			</div>
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<h3 class="panel-title centering">
+						Results
+					</h3>
+				</div>
+				<div class="panel-body" id="lab-result-panel-main">
+					<!-- Searching results should be put here -->
+					<!--
+						recommanded format for author results
+							if not first item in this list:
+								<hr>
+							<div class='panel-item-title-0'>
+								[index].[&nbsp][<a>author name</a>]
+							</div>
+							<div class='panel-item-content-0'>
+								[&nbsp*5]Author ID:[&nbsp][author id][&nbsp*2]-[&nbsp*2]Paper Publish:[&nbsp][total paper]<br>
+								[&nbsp*5]Major Affiliation:[&nbsp][major affiliation]<br>
+							</div>
+					-->
+				</div>
+			</div>
+			<div class="centering">
+				<ul class="pagination" id="lab-result-pagination">
+					<!-- pagination should be put here -->
+				</ul>
+			</div>
+
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-12 centering">
+			<!-- This should be deleted when the pressed event is added to breadcrumb -->
+			<!-- Or the URLs should be changed -->
+			<h3>
+			<?php
+				echo "<a href='".site_url("search/view_home?")."'>Home</a><br>";
+			?>
+			<br>
+			</h3>
+		</div>
+	</div>
+</div>
