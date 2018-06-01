eaef92607f05f9344180ccca3a319cf8f1a0546e
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-06-01 15:55:20 +0800

    directory move

---
+++ application/views/shared/_navi.bar.php
@@ -0,0 +1,47 @@
+<li class="page-item" id="lab-<?php echo $navi_handle; ?>-navi-begin" >
+    <a class="page-link" >&laquo;</a>
+</li>
+
+<li class="page-item
+           <?php echo $pagebackward['status']; ?>
+           <?php echo $pagebackward['active']; ?>"
+    id="lab-<?php echo $navi_handle; ?>-navi-backward"
+    pagenum="<?php echo $pagebackward['number']; ?>"
+    pagestatus="<?php echo $pagebackward['status']; ?>"
+    pageactive="<?php echo $pagebackward['active']; ?>"
+>
+    <a class="page-link" >&lsaquo;</a>
+</li>
+
+<?php foreach($pageindex as $index_item): ?>
+    <li class="page-item
+               <?php echo $index_item['status']; ?>
+               <?php echo $index_item['active']; ?>"
+        id="lab-<?php echo $navi_handle; ?>-navi-item"
+        pagenum="<?php echo $index_item['number']; ?>"
+        pagestatus="<?php echo $index_item['status']; ?>"
+        pageactive="<?php echo $index_item['active']; ?>"
+    >
+        <a class="page-link">
+            <?php echo $index_item['number']; ?>
+        </a>
+    </li>
+<?php endforeach; ?>
+
+<li class="page-item
+           <?php echo $pageforward['status']; ?>
+           <?php echo $pageforward['active']; ?>"
+    id="lab-<?php echo $navi_handle; ?>-navi-forward"
+    pagenum="<?php echo $pageforward['number']; ?>"
+    pagestatus="<?php echo $pageforward['status']; ?>"
+    pageactive="<?php echo $pageforward['active']; ?>"
+>
+    <a class="page-link" >
+        &rsaquo;
+    </a>
+</li>
+
+<li class="page-item"
+    id="lab-<?php echo $navi_handle; ?>-navi-end">
+    <a class="page-link" ?>&raquo;</a>
+</li>
