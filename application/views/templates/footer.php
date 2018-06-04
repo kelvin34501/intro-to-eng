af9a31ad9a1993d491f63f01c7d6ff22b2db6cd1
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-06-04 23:48:05 +0800

    footer update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,9 +2,10 @@
     <div class="container">
 	<div class="row clearfix">
 		<div class="col-md-12 centering text-gray">
-		<br><br>
-		<p>Designed an built with all the love in the world by Xinyu Zhan, Zhongye Wang, Yichen Xie, Ye Yue.</p>
-		<br>
+		<br><br><?php if($title!="Home") echo "<br><br>" ?>
+		<p>Designed and built with all the love in the world by Xinyu Zhan, Zhongye Wang, Yichen Xie, Ye Yue.</p>
+		<p>Thanks for the breathtaking wallpapers by <a href="https://angelus-hellion.deviantart.com">Angelus-Hellion</a></p>
+		<br><?php if($title!="Home") echo "<br><br>" ?>
 		</div>
 	</div>
     </div>
